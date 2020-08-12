42e4349e728baaf461a1c58e39b81d9e4503a30e
Rodrigo Normando <r.normando@hotmail.com>
2020-08-12 16:14:24 +0200

    Updates

---
+++ precache-manifest.4c59cd526bee64f3329b27e93b9b30c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "079885d75a35080e4bbb39ceaab2ed80",
+    "url": "/css-art/index.html"
+  },
+  {
+    "revision": "b834cbc1299185293ce5",
+    "url": "/css-art/static/css/main.48e4cc20.chunk.css"
+  },
+  {
+    "revision": "fbd0548732426c3a952f",
+    "url": "/css-art/static/js/2.e688b635.chunk.js"
+  },
+  {
+    "revision": "b834cbc1299185293ce5",
+    "url": "/css-art/static/js/main.aea30e6a.chunk.js"
+  },
+  {
+    "revision": "3c24d4cab9330acba742",
+    "url": "/css-art/static/js/runtime-main.2435666d.js"
+  }
+]);
