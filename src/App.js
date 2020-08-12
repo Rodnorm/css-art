91fd54ae0e555aca167d25f31a408a1b8cd2bd86
Rodrigo Normando <r.normando@hotmail.com>
2020-08-12 16:20:55 +0200

    eye

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Keyboard from './arts/Keyboard/Keyboard';
 import Quico from './arts/Quico/Quico';
+import Eyes from './arts/Eyes/Eyes';
 
 function App() {
   return (
     <div className="App">
       <Keyboard />
       <Quico />
+      <Eyes />
     </div>
   );
 }
---
+++ src/arts/Eyes/Eyes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Eyes.scss';
+
+export default function Eyes() {
+    return (
+        <div className="eyes-holder">
+            <div className="eyes"></div>
+        </div>
+    )
+}
